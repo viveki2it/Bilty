aaf6a58220fa9702c87a5c2caee31513f6630ebd
rails <rails@rails.(none)>
2014-02-13 16:11:51 +0530

    Modification on dashboard part

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,8 +4,11 @@ class DashboardsController < ApplicationController
       if params[:to_date].to_date >  params[:from_date].to_date
         #        if (params[:to_date].to_date - params[:from_date].to_date).to_i > 90
         @data = (params[:from_date].to_datetime.beginning_of_day..params[:to_date].to_datetime.end_of_day).map { |date| current_user.lr_entries.total_on(date).to_f}.inspect
+        @lorry_challan_data = (params[:from_date].to_datetime.beginning_of_day..params[:to_date].to_datetime.end_of_day).map { |date| current_user.lorry_challans.total_on(date).to_f}.inspect
         @pointInterval = 1.day.to_i * 1000
         @pointStart = params[:from_date].to_datetime.beginning_of_day.to_i * 1000 
+        @lr_entries = current_user.lr_entries.where("created_at BETWEEN ? AND ?", params[:from_date].to_datetime.beginning_of_day, params[:to_date].to_datetime.end_of_day)
+        @lorry_callans = current_user.lorry_challans.where("created_at BETWEEN ? AND ?", params[:from_date].to_datetime.beginning_of_day, params[:to_date].to_datetime.end_of_day)
         #        else
         #          @days_def = ((params[:to_date].to_datetime - params[:from_date].to_datetime).to_i/30).to_i
         #          @data = (params[:from_date].to_datetime.beginning_of_day..params[:to_date].to_datetime.end_of_day).map { |date| current_user.lr_entries.total_on(date).to_f}.inspect
@@ -17,6 +20,9 @@ class DashboardsController < ApplicationController
       end
     else
       @data = (1.month.ago.to_datetime.beginning_of_day..Time.zone.now.end_of_day).map { |date| current_user.lr_entries.total_on(date).to_f}.inspect
+      @lorry_challan_data = (1.month.ago.to_datetime.beginning_of_day..Time.zone.now.end_of_day).map { |date| current_user.lorry_challans.total_on(date).to_f}.inspect
+      @lr_entries = current_user.lr_entries.where("created_at BETWEEN ? AND ?", 1.month.ago.to_datetime.beginning_of_day, Time.zone.now.end_of_day)
+      @lorry_callans = current_user.lorry_challans.where("created_at BETWEEN ? AND ?", 1.month.ago.to_datetime.beginning_of_day, Time.zone.now.end_of_day)
       @pointInterval = 1.day * 1000
       @pointStart = 1.month.ago.at_midnight.to_i * 1000
     end
--- app/models/lorry_challan.rb
+++ app/models/lorry_challan.rb
@@ -1,3 +1,7 @@
 class LorryChallan < ActiveRecord::Base
   belongs_to :user
+  
+  def self.total_on(date)
+    where("date(created_at) = ?",date).count
+  end
 end
